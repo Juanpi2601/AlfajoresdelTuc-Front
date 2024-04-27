bdf13338b9727f646baf2fcc72a9863cf1a03f8b
Juanpi2601 <juanpablofortuny89@gmail.com>
2024-04-27 13:47:23 -0300

    modificanco pagina principal reproduccion sola y corrigiendo errores

--- src/components/paginaPrincipal/PaginaPrincipalV1.jsx
+++ src/components/paginaPrincipal/PaginaPrincipalV1.jsx
@@ -1,29 +1,35 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import '../paginaPrincipal/paginaPrincipal.css'
 import { Container, Row, Carousel, Card, CardText } from 'react-bootstrap'
 
-
 const PaginaPrincipalV1 = () => {
+    
+    const videosRefs = [useRef(null), useRef(null), useRef(null)];
+
+    const handleVideoEnd = (index) => {
+        if (videosRefs[index].current) {
+            videosRefs[index].current.play();
+        }
+    };
     return (
         <>
             <Container>
                 <Row>
-                    <section className='section1 mt-5'>
+                    <section className='section1 mt-5 p-0'>
                         <Carousel>
-                            <Carousel.Item interval={10000000}>
-                                <video controls className='video1'>
+                            <Carousel.Item interval={10500}>
+                                <video ref={videosRefs[0]} autoPlay muted className='video1 w-100 ' onEnded={() => handleVideoEnd(0)}>
                                     <source src='/src/assets/alfajor1.mp4' type='video/mp4' />
                                 </video>
                             </Carousel.Item>
-                            <Carousel.Item interval={500}>
-                                <video autoPlay className='video1' >
+                            <Carousel.Item interval={6000}>
+                                <video ref={videosRefs[1]} autoPlay muted className='video1 w-100'onEnded={() => handleVideoEnd(1)} >
                                     <source src='/src/assets/Colacion.mp4' type='video/mp4' />
-
                                 </video>
 
                             </Carousel.Item>
-                            <Carousel.Item>
-                                <video autoPlay className='video1'>
+                            <Carousel.Item interval={11000}>
+                                <video ref={videosRefs[2]} autoPlay muted className='video1 w-100' onEnded={() => handleVideoEnd(2)}>
                                     <source src='/src/assets/Conitos.mp4' type='video/mp4' />
                                 </video>
                             </Carousel.Item>
@@ -34,37 +40,39 @@ const PaginaPrincipalV1 = () => {
                         <section className='mt-5'>
                             <Card style={{ width: '18rem' }} className='card1'>
                                 <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                <CardText className='cardText'>
+                                <div className='cardText'>
                                     <h4 className='text1'>E-comerce</h4>
                                     <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                </CardText>
+                                </div>
                             </Card>
                         </section>
                         <section className='mt-5'>
                             <Card style={{ width: '18rem' }} className='card1'>
                                 <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                <CardText className='cardText'>
+                                <div className='cardText'>
                                     <h4 className='text1'>E-comerce</h4>
                                     <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                </CardText>
+                                </div>
                             </Card>
                         </section>
                         <section className='mt-5'>
                             <Card style={{ width: '18rem' }} className='card1'>
                                 <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                <CardText className='cardText'>
+                                <div className='cardText'>
                                     <h4 className='text1'>E-comerce</h4>
                                     <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                </CardText>
+                                </div>
                             </Card>
                         </section>
                         <section className='mt-5'>
                             <Card style={{ width: '18rem' }} className='card1'>
                                 <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                <CardText className='cardText'>
+                                <div className='cardText'>
                                     <h4 className='text1'>E-comerce</h4>
                                     <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                </CardText>
+                                </div>
+                                
+                                
                             </Card>
                         </section>
                     </section>
@@ -81,4 +89,4 @@ const PaginaPrincipalV1 = () => {
     )
 }
 
-export default PaginaPrincipalV1
+export default PaginaPrincipalV1;
