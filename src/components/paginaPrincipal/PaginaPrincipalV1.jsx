f545e41fa60f2710de16a2b5194820ac36a4cc72
Nahun Melhen <141931060+Nahun-Melhen@users.noreply.github.com>
2024-04-30 13:33:41 -0300

    dandole estilos a la pagina Home

--- src/components/paginaPrincipal/PaginaPrincipalV1.jsx
+++ src/components/paginaPrincipal/PaginaPrincipalV1.jsx
@@ -11,119 +11,73 @@ const PaginaPrincipalV1 = () => {
             videosRefs[index].current.play();
         }
     };
-    
-        return (
-            <>
-                <Container>
-                    <Row>
-                        <section className='section1 mt-5 p-0'>
-                            <Carousel>
-                                <Carousel.Item interval={10500}>
-                                    <video ref={videosRefs[0]} autoPlay muted className='video1 w-100 ' onEnded={() => handleVideoEnd(0)}>
-                                        <source src='/src/assets/alfajor1.mp4' type='video/mp4' />
-                                    </video>
-                                </Carousel.Item>
-                                <Carousel.Item interval={6000}>
-                                    <video ref={videosRefs[1]} autoPlay muted className='video1 w-100' onEnded={() => handleVideoEnd(1)} >
-                                        <source src='/src/assets/Colacion.mp4' type='video/mp4' />
-                                    </video>
 
-                                </Carousel.Item>
-                                <Carousel.Item interval={11000}>
-                                    <video ref={videosRefs[2]} autoPlay muted className='video1 w-100' onEnded={() => handleVideoEnd(2)}>
-                                        <source src='/src/assets/Conitos.mp4' type='video/mp4' />
-                                    </video>
-                                </Carousel.Item>
-                            </Carousel>
+    return (
+        <>
+            <Container>
+                <Row>
+                    <section className='section1 mt-5 p-0'>
+                        <Carousel>
+                            <Carousel.Item interval={10500}>
+                                <video ref={videosRefs[0]} autoPlay muted className='video1 w-100 ' onEnded={() => handleVideoEnd(0)}>
+                                    <source src='/src/assets/alfajor1.mp4' type='video/mp4' />
+                                </video>
+                            </Carousel.Item>
+                            <Carousel.Item interval={6000}>
+                                <video ref={videosRefs[1]} autoPlay muted className='video1 w-100' onEnded={() => handleVideoEnd(1)} >
+                                    <source src='/src/assets/Colacion.mp4' type='video/mp4' />
+                                </video>
 
+                            </Carousel.Item>
+                            <Carousel.Item interval={11000}>
+                                <video ref={videosRefs[2]} autoPlay muted className='video1 w-100' onEnded={() => handleVideoEnd(2)}>
+                                    <source src='/src/assets/Conitos.mp4' type='video/mp4' />
+                                </video>
+                            </Carousel.Item>
+                        </Carousel>
+
+                    </section>
+                    <section className='cardContainer cards2'>
+                        <section className='mt-5'>
+                            <Card style={{ width: '18rem' }} className='card1 cards3'>
+                                <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
+                                <div className='cardText'>
+                                    <h4 className='text1'><img src="/src/assets/E-comerce.png" alt="E-Comerce" className='img3' /></h4>
+                                    <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
+                                </div>
+                            </Card>
                         </section>
-                        <section className='cardContainer cards2'>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-                                </Card>
-                            </section>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-                                </Card>
-                            </section>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-                                </Card>
-                            </section>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-
-
-                                </Card>
-                            </section>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-                                </Card>
-                            </section>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-                                </Card>
-                            </section>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-                                </Card>
-                            </section>
-                            <section className='mt-5'>
-                                <Card style={{ width: '18rem' }} className='card1 cards3'>
-                                    <Card.Img variant="top" src="/src/assets/Nuestro e-Comercve.png" className='img1' />
-                                    <div className='cardText'>
-                                        <h4 className='text1'>E-comerce</h4>
-                                        <p className='text-white'>Conoce nuestra nueva tienda de Productos</p>
-                                    </div>
-                                </Card>
-                            </section>
+                        <section className='mt-5'>
+                            <Card style={{ width: '18rem' }} className='card1 cards3'>
+                                <Card.Img variant="top" src="/src/assets/Sucursales.png" className='img1' />
+                                <div className='cardText'>
+                                    <h4 className='text1'><img src="/src/assets/Sucursales2.png" alt="Sucursales solo" className='img2' /></h4>
+                                    <p className='text-white'>Conoce donde se ubican nuestras sucursales de productos regionales</p>
+                                </div>
+                            </Card>
                         </section>
-                    </Row>
-                </Container>
+                        <section className='mt-5'>
+                            <Card style={{ width: '18rem' }} className='card1 cards3'>
+                                <Card.Img variant="top" src="/src/assets/Sucursales.png" className='img1' />
+                                <div className='cardText'>
+                                    <h4 className='text1'>Sucursales</h4>
+                                    <p className='text-white'>Conoce donde se ubican nuestras sucursales de productos regionales</p>
+                                </div>
+                            </Card>
+                        </section>
+
+                    </section>
+                </Row>
+            </Container>
 
 
-                <h1 className='mt-5'>FOOTER</h1>
+            <h1 className='mt-5'>FOOTER</h1>
 
 
 
 
-            </>
-        )
-    }
+        </>
+    )
+}
 
-    export default PaginaPrincipalV1;
+export default PaginaPrincipalV1;
