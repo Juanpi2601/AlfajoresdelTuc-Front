60416524bd53a4983d3548694c2628c268ab29f8
Emiliano Acosta <emilianoacosta18@outlook.com>
2024-05-03 10:45:39 -0300

    Se agrega Panel de Productos

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -5,9 +5,14 @@ import { useNavigate } from "react-router-dom";
 const Admin = () => {
   const navigate = useNavigate()
   return (
+    <>
     <Button variant="link" onClick={() => navigate("/admin/usuarios")} className="m-5">
       Panel de Usuarios
     </Button>
+    <Button variant="link" onClick={() => navigate("/admin/productos")} className="m-5">
+      Panel de Productos
+    </Button>
+    </>
   )
 }
 
