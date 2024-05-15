da9b421b5d93903bb18535b6116e9c89a4ad475e
Emiliano Acosta <emilianoacosta18@outlook.com>
2024-05-15 16:56:27 -0300

    Se agregan mensajes de validaciones al panel de productos

--- src/components/paginaProductos/PanelProductos.jsx
+++ src/components/paginaProductos/PanelProductos.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Form, InputGroup, Button, Table } from 'react-bootstrap';
+import { Container, Form, InputGroup, Button, Table,Alert  } from 'react-bootstrap';
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import { alertCustom, alertConfirm } from '../../utils/alertCustom';
 import { useProductAuth } from '../../context/ProductContext';
 import PaginationRounded from "../pagination/Pagination";
+import ErrorIcon from '@mui/icons-material/Error';
 
 const PanelProductos = () => {
     const { signin, productos, getAllProduct, deleteProduct, editProduct } = useProductAuth();
@@ -14,6 +15,14 @@ const PanelProductos = () => {
         imagenUrl: '',
         descripcion: '',
         cantidad:'',
+        categoria: ''
+    });
+    const [errors, setErrors] = useState({
+        nombre: '',
+        precio: '',
+        imagenUrl: '',
+        descripcion: '',
+        cantidad: '',
         categoria: ''
     });
     const [editIndex, setEditIndex] = useState(null);
@@ -52,14 +61,23 @@ const PanelProductos = () => {
             ...prevState,
             [name]: value
         }));
+        setErrors(prevErrors => ({
+            ...prevErrors,
+            [name]: ''
+        }));
     };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         const { nombre, precio, imagenUrl, descripcion, cantidad, categoria } = formData;
+        const validationErrors = validateForm();
         try {
             if (!nombre || !precio || !imagenUrl || !descripcion|| !cantidad || !categoria) {
                 alertCustom("Por favor, completa todos los campos.");
+                return;
+            }
+            if (Object.values(validationErrors).some(error => error !== '')) {
+                setErrors(validationErrors);
                 return;
             }
             if (editIndex !== null) {
@@ -107,6 +125,30 @@ const PanelProductos = () => {
         setSubmitButtonText('Editar Producto');
     };
 
+    const validateForm = () => {
+        const { nombre, precio, imagenUrl, descripcion, cantidad, categoria } = formData;
+        const errors = {
+            nombre: nombre ? '' : 'Por favor, introduce un nombre.',
+            precio: precio ? (isValidPrice(precio) ? '' : 'El precio debe ser un número positivo.') : 'Por favor, introduce un precio.',
+            imagenUrl: imagenUrl ? (isValidImageUrl(imagenUrl) ? '' : 'La URL de la imagen debe ser válida y tener una extensión .jpg, .jpeg o .png.') : 'Por favor, introduce una URL de imagen.',
+            descripcion: descripcion ? '' : 'Por favor, introduce una descripción.',
+            cantidad: cantidad ? (isValidQuantity(cantidad) ? '' : 'La cantidad debe ser un número positivo.') : 'Por favor, introduce una cantidad.',
+            categoria: categoria ? '' : 'Por favor, selecciona una categoría.'
+        };
+        return errors;
+    };
+    const isValidImageUrl = (url) => {
+        const imageUrlRegex = /\.(jpeg|jpg|png)$/;
+        return imageUrlRegex.test(url);
+    };
+    const isValidPrice = (price) => {
+        return !isNaN(price) && parseFloat(price) > 0 && parseFloat(price) <10000000;
+    };
+    
+    const isValidQuantity = (quantity) => {
+        return !isNaN(quantity) && parseFloat(quantity) > 0;
+    };
+
     const totalPages = Math.ceil(productos.length / itemsPerPage);
 
     const indexOfLastItem = currentPage * itemsPerPage;
@@ -125,13 +167,17 @@ const PanelProductos = () => {
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formGroupNombre">
                         <Form.Control type="text" name="nombre" placeholder="Ingresar Nombre del Producto" value={formData.nombre} onChange={handleChange} />
+                        {errors.nombre && <Alert className='my-3' variant="danger"><ErrorIcon/>{errors.nombre}</Alert>}
                     </Form.Group>
-                    <InputGroup className="mb-3">
+                    <InputGroup className="d-flex mb-3">
                         <InputGroup.Text>$</InputGroup.Text>
                         <Form.Control type="number" name="precio" placeholder="Precio del Producto" aria-label="Amount (to the nearest dollar)" value={formData.precio} onChange={handleChange} />
+                        
                     </InputGroup>
+                    {errors.precio && <Alert className='' variant="danger"><ErrorIcon/>{errors.precio}</Alert>}
                     <Form.Group controlId="formImageUrl" className="mb-3">
                         <Form.Control type="text" name="imagenUrl" placeholder="Ingrese la URL de la imagen" value={formData.imagenUrl} onChange={handleChange} />
+                        {errors.imagenUrl && <Alert className='my-3 ' variant="danger"><ErrorIcon/>{errors.imagenUrl}</Alert>}
                     </Form.Group>
                     <Form.Select className='mb-3' aria-label="Default select example" name="categoria" value={formData.categoria} onChange={handleChange}>
                         <option>Seleccionar Categoría</option>
@@ -139,13 +185,15 @@ const PanelProductos = () => {
                         <option value="Conitos">Conitos</option>
                         <option value="Nueces">Nueces</option>
                         <option value="Cajas">Cajas</option>
-                        <option value="Bombones">Bombones</option>
+                        <option value="Bombones">Bombones</option>\
                     </Form.Select>
                     <Form.Group controlId="formDescripcion" className="mb-3">
                         <Form.Control  type='text' name="descripcion" placeholder="Descripcion del Producto" aria-label="Amount (to the nearest dollar)" value={formData.descripcion} onChange={handleChange} />
+                        {errors.descripcion && <Alert className='my-3' variant="danger"><ErrorIcon/>{errors.descripcion}</Alert>}
                     </Form.Group>
                     <Form.Group>
                         <Form.Control type='text' name="cantidad" placeholder='Cantidad del Producto' aria-label="Amount (to the nearest dollar)" value={formData.cantidad} onChange={handleChange}/>
+                        {errors.cantidad && <Alert className='my-3' variant="danger"><ErrorIcon/>{errors.cantidad}</Alert>}
                     </Form.Group>
                     <Button type="submit" className='my-3'>{editIndex !== null ? 'Editar Producto' : 'Agregar Producto'}</Button>
                 </Form>
