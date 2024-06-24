af212fe53afcbc2dfa74ec9d8e33bde303cf5e19
francoasc <francoascarate349@gmail.com>
2024-06-24 22:55:25 +0200

    chore: Update axios configuration to include token in request headers

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import Cookies from "js-cookie";
 const URL_BASE = import.meta.env.VITE_URL_BASE;
 
 const instance = axios.create({
@@ -8,5 +9,17 @@ const instance = axios.create({
     "Content-Type": "application/json",
   },
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers["Cookie"] = `token=${token}`;
+      config.headers["cookie"] = `token=${token}`;
+    }
+    return config;
+  },
+  (error) => error
+);
 
 export default instance;
