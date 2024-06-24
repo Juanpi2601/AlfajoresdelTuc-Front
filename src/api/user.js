ebe56f1999aae0dd6a67761cde1f5a6381edd187
Nahun Melhen <141931060+Nahun-Melhen@users.noreply.github.com>
2024-06-24 11:47:34 -0300

    changes login mudafuker

--- src/api/user.js
+++ src/api/user.js
@@ -2,6 +2,17 @@ import axios from "./axios";
 
 export const registerRequest = (user) => axios.post(`user/create`, user);
 
-export const verifyTokenRequest = () => axios.get('user/verifyToken');
-
+export const verifyTokenRequest = async (token) => {
+    try {
+      const res = await axios.post('/user/verify-token', {}, {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      });
+      return res;
+    } catch (error) {
+      throw error;
+    }
+  };
+  
 export const updatePasswordRequest = (user) => axios.patch('user/update-password', user);
