e554ce6fa762030003d77cfa983c996fb8ecf651
Juanpi2601 <juanpablofortuny89@gmail.com>
2024-06-25 20:19:43 -0300

    cambiando axios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,12 +1,22 @@
-import axios from "axios";
+import axios from 'axios';
+import Cookies from 'js-cookie';
 const URL_BASE = import.meta.env.VITE_URL_BASE;
+
 
 const instance = axios.create({
   baseURL: URL_BASE,
   withCredentials: true,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
+    'Accept': '/',
+    'Cache-Control': 'no-cache',
   },
 });
+
+
+const token = Cookies.get('token');
+if (token) {
+  instance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
 
 export default instance;
